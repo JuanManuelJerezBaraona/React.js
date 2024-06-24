81d35a2b63c5982ff86bd8936055c61138da4993
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-06-24 08:27:25 -0400

    chore: Add FocusScreen component to main.jsx

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,6 +6,7 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 import './index.css';
 
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <CounterWithCustomHook /> */}
     {/* <SimpleForm /> */}
     {/* <FormWithCustomHook /> */}
-    <MultipleCustomHooks />
+    {/* <MultipleCustomHooks /> */}
+    <FocusScreen />
   </React.StrictMode>,
 )
