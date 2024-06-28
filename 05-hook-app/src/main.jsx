7c04eedb48342fd98ac91fa83be7c96a1969bc2c
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-06-28 12:49:37 -0400

    intro-reducer.js

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -11,19 +11,21 @@ import ReactDOM from 'react-dom/client';
 // import { Memorize } from './06-memos/Memorize';
 import { Padre } from './07-tarea-memo/Padre';
 
+import './08-useReducer/intro-reducer';
+
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <HooksApp /> */}
-    {/* <CounterApp /> */}
-    {/* <CounterWithCustomHook /> */}
-    {/* <SimpleForm /> */}
-    {/* <FormWithCustomHook /> */}
-    {/* <MultipleCustomHooks /> */}
-    {/* <FocusScreen /> */}
-    {/* <Layout /> */}
-    {/* <Memorize /> */}
-    <Padre />
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     {/* <HooksApp /> */}
+//     {/* <CounterApp /> */}
+//     {/* <CounterWithCustomHook /> */}
+//     {/* <SimpleForm /> */}
+//     {/* <FormWithCustomHook /> */}
+//     {/* <MultipleCustomHooks /> */}
+//     {/* <FocusScreen /> */}
+//     {/* <Layout /> */}
+//     {/* <Memorize /> */}
+//     <Padre />
+//   </React.StrictMode>,
+// )
