67619cfe639d2b1d18b29f280e2184286d39d586
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-06-30 09:45:00 -0400

    chore: Update UserContext usage in HomePage and LoginPage components

--- 05-hook-app/src/09-useContext/HomePage.jsx
+++ 05-hook-app/src/09-useContext/HomePage.jsx
@@ -1,9 +1,18 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
 
 export const HomePage = () => {
+
+    const {user} = useContext(UserContext);
+
     return (
         <>
             <h1 className="text-center mt-2">HomePage</h1>
             <hr />
+
+            <pre>
+                {JSON.stringify(user, null, 3)}
+            </pre>
         </>
     )
 }
--- 05-hook-app/src/09-useContext/LoginPage.jsx
+++ 05-hook-app/src/09-useContext/LoginPage.jsx
@@ -1,9 +1,29 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
 
 export const LoginPage = () => {
+
+    const {user, setUser} = useContext(UserContext);
+
     return (
         <>
             <h1 className="text-center mt-2">LoginPage</h1>
             <hr />
+            
+            <pre>
+                {JSON.stringify(user, null, 3)}
+            </pre>
+
+            <button 
+                onClick={() => setUser({
+                    id: 123,
+                    name: 'Juan',
+                    email: 'juan@gmail.com'
+                })}
+                className="btn btn-primary"
+            >
+                Establecer usuario
+            </button>
         </>
     )
 }
